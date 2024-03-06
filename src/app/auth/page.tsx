538ecff486d36a7b2ae8908d1f36dd9a280d6d04
mohits-git <mohitkhatri2805@gmail.com>
2024-03-06 23:50:54 +0530

    added clerk for user management

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,11 +1,16 @@
-import { UserButton } from "@clerk/nextjs";
+'use client';
+import { SignInButton, SignOutButton, UserButton, useUser } from "@clerk/nextjs";
 
-export default async function Home() {
+export default function Auth() {
+    const user = useUser();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <UserButton />
+      {!user.isSignedIn && <SignInButton />}
+       {!!user.isSignedIn && <UserButton />}
+        
       </div>
     </main>
   );
 }
+
