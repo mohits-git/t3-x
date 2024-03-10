a06406a71bd6f566fb697d747acda2b23b1bc577
mohits-git <mohitkhatri2805@gmail.com>
2024-03-10 20:49:56 +0530

    fix: nested link (<a> tags)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,26 +1,71 @@
 import { type Metadata } from "next";
 import ProfileView from "../_components/ProfileView";
+// import { createServerSideHelpers } from '@trpc/react-query/server';
+// import superjson from 'superjson';
+// import { type GetServerSidePropsContext } from "next";
+// import { appRouter } from '@/server/api/root';
+// import { createTRPCContext } from '@/server/api/trpc';
+// import { TRPCError } from '@trpc/server';
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata({ params }: Props ): Promise<Metadata> {
+  const { slug } = params;
   return {
-    title: "Profile",
+    title: `Profile (@${slug})`,
   }
 }
 
-type Params = {
+type Props = {
   params: {
     slug: string,
   },
 }
 
-export default function ProfilePage({ params }: Params) {
+export default function ProfilePage(
+  { params }: Props 
+) {
   const { slug } = params;
-
   return (
     <main className="container mx-auto flex min-h-screen flex-col items-center justify-center">
       <div className="w-full min-h-screen max-w-2xl border-x border-x-white/30">
-        <ProfileView userId={slug}/>
+        <ProfileView userId={slug} />
       </div>
     </main>
   )
 }
+
+
+// we don' t need all these prefetching pain, this was only needed in page router but not in app router
+
+// export async function getStaticProps(
+//   context: GetServerSidePropsContext<{ slug: string }>,
+// ) {
+//   const helpers = createServerSideHelpers({
+//     router: appRouter,
+//     ctx: await createTRPCContext({ headers: new Headers() }),
+//     transformer: superjson,
+//   });
+//
+//   const userId = context.params?.slug;
+//
+//   if (!userId) throw new TRPCError({ code: "BAD_REQUEST" })
+//
+//   await helpers.profile.getProfile.prefetch({ userId });
+//
+//   return {
+//     props: {
+//       trpcState: helpers.dehydrate(),
+//       userId,
+//     }
+//   }
+//
+// }
+//
+// export async function generateStaticParams() {
+//   return [ { slug: 'user_2dKDooyqqMF1nAyXfBiaTDbHSV8' }];
+// }
+//
+// // export const getStaticPaths = () => {
+// //   return {
+// //     paths: [], fallback: "blocking",
+// //   }
+// // }
--- src/app/_components/PostView.tsx
+++ src/app/_components/PostView.tsx
@@ -11,33 +11,35 @@ dayjs.extend(relativeTime);
 export default function PostView({ post, author }: PostWithUser) {
   return (
     <div className="p-6 w-full bg-white/5 border-y border-y-white/10 space-y-2">
-      <Link href={`/post/${post.id}`}>
-        <div className="flex items-start space-x-3">
-          <Link href={`/${author.id}`}>
-            <div className="rounded-full max-w-10 max-h-10 overflow-hidden shrink-0">
-              <Image
-                src={author.profileImageUrl ?? "/favicon.ico"}
-                alt={`@${author.username}'s profile picture`}
-                width={40}
-                height={40}
-              />
-            </div>
-          </Link>
-          <div>
-            <div className="gap-1 flex text-sm text-muted">
-              <Link href={`/${author.id}`}>
-                <span className="font-medium text-white">{author.name}</span>
-              </Link>
-              <Link href={`/${author.id}`}>
-                <span>{`@${author.username}`}</span>
-              </Link>
-              <span>{`·`}</span>
-              <span>{dayjs(post.createdAt).fromNow()}</span>
-            </div>
-            <p>{post.content}</p>
+      <div className="flex items-start space-x-3">
+        <Link href={`/${author.id}`}>
+          <div className="rounded-full max-w-10 max-h-10 overflow-hidden shrink-0">
+            <Image
+              src={author.profileImageUrl ?? "/favicon.ico"}
+              alt={`@${author.username}'s profile picture`}
+              width={40}
+              height={40}
+            />
           </div>
+        </Link>
+        <div>
+          <div className="gap-1 flex text-sm text-muted">
+            <Link href={`/${author.id}`}>
+              <span className="font-medium text-white">{author.name}</span>
+            </Link>
+            <Link href={`/${author.id}`}>
+              <span>{`@${author.username}`}</span>
+            </Link>
+            <span>{`·`}</span>
+            <Link href={`/post/${post.id}`}>
+              <span>{dayjs(post.createdAt).fromNow()}</span>
+            </Link>
+          </div>
+          <Link href={`/post/${post.id}`}>
+            <p>{post.content}</p>
+          </Link>
         </div>
-      </Link>
+      </div>
     </div>
   )
 }
