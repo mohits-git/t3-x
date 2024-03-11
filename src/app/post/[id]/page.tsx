d09a3f9fe5ab54626fdaf7950ccb30ddcbebe04c
mohits-git <mohitkhatri2805@gmail.com>
2024-03-11 18:12:08 +0530

    feat: added single post view page

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,13 +1,40 @@
+import PostView from "@/app/_components/PostView";
+import { api } from "@/trpc/server";
 import { type Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: "Post"
+type Props = {
+  params: {
+    id: string,
+  },
 }
 
-export default function PostPage() {
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = params;
+  const data = await api.post.getPostById.query({ postId: id });
+
+  if (!data) {
+    return {
+      title: "Post Not Found"
+    }
+  }
+  return {
+    title: `${data.post.content} | @${data.author.username}`
+  }
+}
+
+export default async function PostPage({ params }: Props) {
+  const { id } = params;
+  const data = await api.post.getPostById.query({ postId: id });
+
+  if (!data) {
+    return <div className="w-full flex justify-center">
+      Could&apos;nt load the post
+    </div>
+  }
+
   return (
-    <div>
-      Post View
+    <div className="w-full flex justify-center">
+      <PostView {...data} />
     </div>
   )
 }
